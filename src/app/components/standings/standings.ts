d51c7b6e18d32c2200556069fa58b56dc63c0338
desainr <eniiseekay@gmail.com>
2018-10-19 00:42:40 -0400

    Completed MVP. Ability to save a game, have it reflect in the standings.

---
+++ src/app/components/standings/standings.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+import {Player} from "../../../players/player.model";
+import {PlayerService} from "../../../players/player.service";
+
+@Component({
+  selector: 'StandingsTable',
+  templateUrl: 'standings.html'
+})
+export class StandingsComponent {
+
+  public players: Player[];
+
+  constructor(private playerService: PlayerService) {
+    this.playerService.GetAll().subscribe((players: Player[]) => {
+      this.players = players.sort((p1, p2) => {
+        return p2.WinPct - p1.WinPct
+      });
+    })
+  }
+}
