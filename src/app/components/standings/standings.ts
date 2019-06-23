bba40e5bd02743530678c14ce15b3553711fae9e
Robert Kump <kumprj@miamioh.edu>
2019-06-23 19:29:41 -0400

    updated get win pct

--- src/app/components/standings/standings.ts
+++ src/app/components/standings/standings.ts
@@ -13,19 +13,12 @@ export class StandingsComponent {
 
   constructor(private playerService: PlayerService) {
     this.playerService.GetAll().subscribe((players: Player[]) => {
-      console.log("getwinpct error")
-      console.log(players)
-      console.log(typeof players)
       this.players = players.sort((p1, p2) => {
 
         let player1 = Object.assign(new Player(), p1);        
         let player2 = Object.assign(new Player(), p2);
         p1.winPct = parseFloat((player1.getWinPct()*100).toFixed(2))
         p2.winPct = parseFloat((player2.getWinPct()*100).toFixed(2))
-
-        console.log("list players")
-        console.log(p1.Name + " " + p1.Wins + " " + p1.Losses)
-        console.log(p2.Name + " " + p2.Wins + " " + p2.Losses)
         return player2.getWinPct() - player1.getWinPct()        
       });
     });
